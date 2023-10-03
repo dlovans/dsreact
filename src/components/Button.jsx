431a0451083ed9bb6a84956de1f3de182b539270
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2023-10-04 00:21:05 +0200

    Create ripple effect for button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,17 @@
 import '../../public/css/Button.css'
 
 export default function Button() {
+    const genericButton = document.querySelector('.button')
+    genericButton.addEventListener('click', function () {
+        genericButton.classList.add('ripple')
+        setTimeout(() => {
+            genericButton.classList.remove('ripple')
+        }, 400)
+        genericButton.classList.add()
+    })
     return (
         <>
-            <button className="button">Contact Me</button>
+            <button className="button">Contact Me<span className='ripple'></span></button>
         </>
     )
 }
